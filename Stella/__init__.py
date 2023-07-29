8593b2933faf2cf0bf67dcadde67a7c70f6bfd25
shiwa786 <74191978+shiwa786@users.noreply.github.com>
2023-07-29 23:23:45 +0530

    Update __init__.py

--- Stella/__init__.py
+++ Stella/__init__.py
@@ -20,7 +20,7 @@ import sys
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pymongo import MongoClient
 from pyrogram import Client
-from pyromod import listen
+from Pyroaddon import listen
 from aiohttp import ClientSession
 
 from Stella.config import Config
