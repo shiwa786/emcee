aeb8c2e9f8694fefb28ccdbfb6b4a3f6a7c99558
shiwa786 <74191978+shiwa786@users.noreply.github.com>
2023-08-02 18:01:25 +0530

    Update config.py

--- Stella/config.py
+++ Stella/config.py
@@ -31,7 +31,7 @@ class Config:
 
     OWNER_ID = int(getenv("OWNER_ID", "5497627952"))
     
-    TOKEN = getenv("TOKEN", "5643681960:AAEmdu_fXu-QaiQx0lBX9skXLyMQgQzjfRY")
+    BOT_TOKEN = getenv("TOKEN", "5643681960:AAEmdu_fXu-QaiQx0lBX9skXLyMQgQzjfRY")
     BOT_NAME = getenv("BOT_NAME", "Elisa")
     BOT_USERNAME= getenv("BOT_USERNAME", "AlexamachineRobot")
     BOT_ID = getenv("BOT_ID", "5643681960")
